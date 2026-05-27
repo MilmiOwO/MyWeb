ea0aacd5717b6bcdc8e7253d8e1412e6177fcead
MilmiOwO <a87330514@gmail.com>
2026-05-27 17:32:42 +0900

    fuck

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask, request, session, render_template
 from argon2 import PasswordHasher
 import subprocess
 
 app = Flask(__name__)
+app.secret_key = open('env/secret_key.txt').read().strip()
 port = 5000
 
 ph = PasswordHasher()
@@ -17,34 +19,34 @@ idx = lines.index('Wireless LAN adapter Wi-Fi:')
 line = lines[idx + 4]
 ip = line.split()[-1]
 print(ip + ':' + str(port))
+
 @app.route('/')
 def index():
-    if session.get('is_admin'):
-        return render_template('index.html', message = 'hello, admin!')
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/requestbin')
 def requestbin():
     return "coming soon"
 
-@app.route('/get_authority', methods=['GET', 'POST'])
-def get_authority():
-    if request.method == 'GET':
-        if session.get('is_admin') == True:
-            return "you already have permission!"
-        else:
-            return render_template("auth.html")
-    elif request.method == 'POST':
-        admin_pwHash = open('adminPasswordHash.txt', 'r').read().strip()
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        admin_pwHash = open('env/adminPasswordHash.txt', 'r').read().strip()
         pw = request.form.get('password')
         hash = ph.hash(pw)
+        print(pw)
+        print(hash)
+        print(admin_pwHash)
         if hash == admin_pwHash:
             session['is_admin'] = True
-            return render_template("index.html")
+            return render_template('index.html', alert = 'login success - Hello, admin!')
         else:
-            return render_template("auth.html")
-
+            return render_template('auth.html', error = 'wrong password')
+    else:
+        if session.get('is_admin') == True:
+            return render_template('index.html', alert = 'you already have a permission!')
+        else:
+            return render_template('auth.html')
 
 
 if __name__ == '__main__':
--- dev.py
+++ dev.py
@@ -3,3 +3,4 @@ from argon2 import PasswordHasher
 ph = PasswordHasher()
 
 print(ph.hash('tnwlsl98@'))
+print('$argon2id$v=19$m=65536,t=3,p=4$sPIc8jhcTU5/gvx2ZeymBA$oaYq8KyFEb+xI25EJmUsTRWI9Rn+RTlm1X5cSELQxIs')
